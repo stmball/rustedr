351cd8ecf57c80996c0663fa8a2481d46f988621
Samuel Ball <stmball@gmail.com>
2022-01-06 09:00:05 +0000

    This commit does something!
    I think we're starting the parsing function. Although it needs
    refactoring already!

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,13 @@ use std::fmt;
 pub fn run(filename: &String) -> Result<(), Box<dyn Error>> {
 
     let contents = read_edr(&filename)?;
-
-    Ok(())
+    if contents.len() < 2048 {
+        Err(Box::new(ParseError::FileTooShort))
+    } else {
+        let header : &[u8] = &contents[..2048];
+        let header_meta = parse_header(header);
+        Ok(())
+    }
 }
 
 fn read_edr(filename: &String) -> Result<Vec<u8>, ReadError> {
@@ -21,11 +26,26 @@ fn read_edr(filename: &String) -> Result<Vec<u8>, ReadError> {
     }
 }
 
+fn parse_header(header: &[u8]) -> EDRMetadata {
+    let ascii_header: String = header.to_vec().iter().map(|&x| x as char).collect();
+    println!("{}", ascii_header);
+    EDRMetadata { tane : 1}
+}
+
+struct EDRMetadata {
+    tane: i32
+}
+
 #[derive(Debug, PartialEq)]
 enum ReadError {
     EmptyFileName,
     ExtensionError,
     FileReadError(io::ErrorKind),
+}
+
+#[derive(Debug, PartialEq)]
+enum ParseError {
+    FileTooShort,
 }
 
 impl From<io::Error> for ReadError {
@@ -34,7 +54,9 @@ impl From<io::Error> for ReadError {
     }
 }
 
-impl std::error::Error for ReadError {}
+impl Error for ReadError {}
+
+impl Error for ParseError {}
 
 impl fmt::Display for ReadError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -42,6 +64,14 @@ impl fmt::Display for ReadError {
             ReadError::EmptyFileName => write!(f, "empty file name."),
             ReadError::ExtensionError => write!(f, "invalid file extension."),
             ReadError::FileReadError(err) => write!(f, "error reading file, {:?}", err)
+        }
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ParseError::FileTooShort => write!(f, "file contents too short."),
         }
     }
 }
@@ -69,6 +99,11 @@ mod tests {
         let actual_error = read_edr(&String::from("non_existant.EDR"));
         assert!(actual_error.is_err());
         assert_eq!(actual_error.unwrap_err(), ReadError::FileReadError(io::ErrorKind::NotFound))
+    }
+
+    #[test]
+    fn header_too_short () {
+
     }
 }
 
